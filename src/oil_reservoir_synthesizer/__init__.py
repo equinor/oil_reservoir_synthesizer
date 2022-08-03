58843ed3dd704d081ca2fafb39f19300bed65cc1
Eivind Jahren <ejah@equinor.com>
2022-08-03 21:13:38 +0200

    Set up project

--- src/oil_reservoir_synthesizer/__init__.py
+++ src/oil_reservoir_synthesizer/__init__.py
@@ -1,4 +1,15 @@
-from .prime_generator import PrimeGenerator
-from .perlin import PerlinNoise
-from .shaped_perlin import ShapedNoise, ShapeFunction, ShapeCreator
+from pkg_resources import DistributionNotFound, get_distribution
+
 from .oil_simulator import OilSimulator
+
+__author__ = """Equinor"""
+__email__ = "fg_sib-scout@equinor.com"
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = "0.0.0"
+
+__all__ = [
+    "OilSimulator",
+]
