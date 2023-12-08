be9b60026ec1be63d749b9d5a7b3ab41760e101c
Sondre Sortland <sonso@equinor.com>
2023-12-08 11:09:01 +0100

    Use importlib_metadata to get version

--- src/oil_reservoir_synthesizer/__init__.py
+++ src/oil_reservoir_synthesizer/__init__.py
@@ -2,17 +2,13 @@
 This package generates synthetic oil simulator data based
 on perlin noise. See :py:class:`OilSimulator`.
 """
-from pkg_resources import DistributionNotFound, get_distribution
-
+from importlib.metadata import version
 from ._oil_simulator import OilSimulator
 
 __author__ = """Equinor"""
 __email__ = "fg_sib-scout@equinor.com"
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    __version__ = "0.0.0"
+__version__ = version(__name__)
 
 __all__ = [
     "OilSimulator",
